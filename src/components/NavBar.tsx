9b5d6855868dcc9656f35e578c03892c78d2bb8a
aliahmad <aliahmadnaeem@eguanacommerce.com>
2024-10-22 10:56:54 +0500

    revert

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -29,7 +29,7 @@ function Navbar({ className }: { className?: string }) {
                             Songwriting
                         </HoveredLink>
                         <HoveredLink href="/courses">
-                            Music Productions
+                            Music Production
                         </HoveredLink>
                     </div>
                 </MenuItem>
